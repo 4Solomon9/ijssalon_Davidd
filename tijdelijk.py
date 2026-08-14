514c934fa2cc5c8e5b6a46efc96dfa3636ce81cf
solomon <davidromerofeliz2@outlook.com>
2026-08-14 15:00:51 +0200

    bestand tijdelijk.pygecodeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -5,7 +5,6 @@ prijzen = {
 }
 
 aanbieding = prijzen['aardbei'] * 0.8
-
 reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € {aanbieding}"
 reclame_tekst2 = reclame_tekst[:62]
 reclame_tekst3 = reclame_tekst2.upper()
